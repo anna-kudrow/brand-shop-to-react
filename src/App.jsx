caaeb45f00cb74597d414ad49531130ad56790d1
anna <anka-kudrow@mail.ru>
2024-10-05 15:59:00 +0300

    add routing and error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.scss';
+import { Error } from './pages/Error/Error';
 import { IndexPage } from './pages/index/components/IndexPage/IndexPage';
 import { MenProducrPage } from './pages/men-products/components/MenProducrPage/MenProducrPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
-			{/* <IndexPage/> */}
-			<MenProducrPage />
+			<Routes>
+				<Route path="/" element={<IndexPage />} />
+				<Route path="/men-products" element={<MenProducrPage />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
 		</>
 	);
 }
---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,9 @@
+import './Error.scss';
+
+export const Error = () => {
+	return (
+		<div className="error-box">
+			<p className="error-text">{`Ooops... this page doesn't exist :(`}</p>
+		</div>
+	);
+};
