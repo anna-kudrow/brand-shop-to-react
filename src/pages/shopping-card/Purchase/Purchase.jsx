d6e713c5593f31c63f7e00eeaac0cb1732979a37
anna <anka-kudrow@mail.ru>
2024-10-06 15:12:47 +0300

    change size of img of purchase

--- src/pages/shopping-card/Purchase/Purchase.jsx
+++ src/pages/shopping-card/Purchase/Purchase.jsx
@@ -3,7 +3,7 @@ export const Purchase = ({ item }) => {
 		<div className="shopping__item shopping__grid">
 			<div className="grid__left">
 				<a href="#" className="shopping__link">
-					<img src={item.src} alt="black jacket" />
+					<img src={item.src} style={{ width: '100px' }} alt="black jacket" />
 				</a>
 				<div className="grid__discription">
 					<p className="description__name">{item.title}</p>
@@ -17,7 +17,7 @@ export const Purchase = ({ item }) => {
 				</div>
 			</div>
 			<p className="shopping__info">{item.price}</p>
-			<p className="shopping__info">2</p>
+			<p className="shopping__info">1</p>
 			<p className="shopping__info">FREE</p>
 			<p className="shopping__info">...</p>
 			<img className="shopping__info" src="images/cancel.jpg" alt="cancel" />
